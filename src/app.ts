3182c763257d59c5e5721ae5c56ddff60035ffd7
Shubham <shubhamvscode@gmail.com>
2023-11-09 00:25:53 +0530

    Added Path in Response For Error in Development

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import CustomError from "./config/ErrorClass";
 dotenv.config();
 
 import authRouter from "./routes/AuthRoute";
+import connectDB from "./config/db/dbConnect";
 
 const app = express();
 
@@ -22,10 +23,21 @@ app.use("/api/auth", authRouter);
 
 app.use(
   (err: CustomError, _req: Request, res: Response, next: NextFunction) => {
-    res.status(err.statusCode || 500).json({ error: err.message });
+    let json: { error: string; path?: string } = { error: err.message };
+
+    if (process.env.NODE_ENV === "development") {
+      console.log(err);
+      json = {
+        error: err.message,
+        path: _req.url,
+      };
+    }
+
+    res.status(err.statusCode || 500).json(json);
   }
 );
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(">>> Server running on port:", PORT);
+  await connectDB();
 });
