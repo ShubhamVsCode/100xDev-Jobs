4cc003fb9c17d1f44a07b6f7de084a4454de8855
Shubham <shubhamvscode@gmail.com>
2023-11-18 01:27:42 +0530

    refactor: changed env varaiable name for deployment - issue: no-id

--- src/routes/FileRoute.ts
+++ src/routes/FileRoute.ts
@@ -13,8 +13,8 @@ const uploadRouter = Router();
 
 const BUCKET_NAME = process.env.BUCKET_NAME || "100xdev-jobs";
 const AWS_REGION = process.env.AWS_REGION || "ap-south-1";
-const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || "";
-const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || "";
+const AWS_ACCESS_KEY_ID = process.env.AWS_KEY_ID || "";
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_KEY || "";
 
 if (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
   console.error("AWS Credentials not set in env");
